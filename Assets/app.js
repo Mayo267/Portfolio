ba3f2248e7cec80a9228d2edc5a9c3e2c1378c84
Andrew Mayo <andrewmayo@Andrews-MacBook-Pro.local>
2019-01-19 12:40:35 -0500

    button functions

--- Assets/app.js
+++ Assets/app.js
@@ -1,3 +1,28 @@
 $( document ).ready(function() {
+    // About button scrolling
+    $("#nav-button2").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#about-box").offset().top
+        }, 1000);
+    });
+    //Portfolio scrolling
+    $("#nav-button3").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#port-box").offset().top
+        }, 1500);
+    });
+    //Contact scrolling
+    $("#nav-button4").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#contact-box").offset().top
+        }, 2000);
+    });
+    //Back to top button
+    $("#back-top").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#nav").offset().top
+        }, 1500);
+    });
 
+    
 });
